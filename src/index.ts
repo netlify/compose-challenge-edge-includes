8baec6360be58c451aa4121cba0a413a3c83b0f6
Phil Hawksworth <phil@netlify.com>
2024-09-20 12:25:26 +0100

    Moved leaderboard submission to the site repo build plugin

--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,6 @@ extension.addBuildEventHandler('onSuccess', async ({constants}) => {
   if (!extensionConfigured || extensionConfigured === 'false') {
     return;
   }
-
-  // If the site has been deployed, we'll send the score to the leaderboard
-  if (constants.IS_LOCAL) {
-    console.log("Local build. We'll only tell the leaderboard if it's deployed.");
-    return;
-  }
-  await fetch('https://compose-challenge.netlify.app/submission', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      "url": process.env.URL,
-      "score": 1000,
-      "excluded": false
-    })
-  });
 });
 
 extension.addEdgeFunctions('./src/edge-functions', {
